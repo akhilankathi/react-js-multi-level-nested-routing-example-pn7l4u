548947921cadf3fde2b8fab0d30652ce69e6db3b
akhilankathi <91078213+akhilankathi@users.noreply.github.com>
2022-12-09 10:55:25 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import './style.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Leaves from './components/Leaves';
+import ApplyLeaves from './components/ApplyLeaves';
+import CheckLeaveBalance from './components/CheckLeaveBalance';
+import EarnedBalance from './components/EarnedBalance';
+import CasualBalance from './components/CasualBalance';
+export default function App() {
+  return (
+    <div className="content-wrap">
+      <BrowserRouter>
+        <nav>
+          <Link className="navbar-brand" to="dashboard">
+            Dashboard
+          </Link>
+          <Link className="navbar-brand" to="leaves">
+            Leaves
+          </Link>
+        </nav>
+
+        {/* Routes Definition */}
+        <Routes>
+          {/* Parent Navigation Routes */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/leaves" element={<Leaves />}>
+            {/* 2nd Level Navigation Routes */}
+            <Route path="/leaves" element={<ApplyLeaves />} />
+            <Route path="apply-leaves" element={<ApplyLeaves />} />
+            <Route path="check-leave-balance" element={<CheckLeaveBalance />}>
+              {/* 3rd Level Navigation Routes */}
+              <Route
+                path="/leaves/check-leave-balance"
+                element={<CasualBalance />}
+              />
+              <Route path="casual-balance" element={<CasualBalance />} />
+              <Route path="earned-balance" element={<EarnedBalance />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-js-multi-level-nested-routing-example-pn7l4u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-js-multi-level-nested-routing-example-pn7l4u)
